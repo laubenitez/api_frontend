63d2863eb2ac826782e7052e0cde7aeb74e31ca7
Laubenitez <laubenitezr0616@gmail.com>
2026-06-18 13:12:10 -0500

    api+frontend 2.0

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.delete('/productos/nombre/:nombre', productoController.eliminar)
 
 app.get('/clientesvista', async function(req, res) {
     try {
-        const response = await fetch('https://mongo-connection-api2.onrender.com/clientes');
+        const response = await fetch('https://api-frontend-jtxo.onrender.com/clientes');
         const data = await response.json();
 
         res.render('pages/clientes', {
@@ -60,123 +60,6 @@ app.get('/clientesvista', async function(req, res) {
     }
 });
 
-// Registrar cliente
-// mostrar formulario
-app.get('/clientes/nuevo', function(req, res) {
-    res.render('pages/nuevoCliente');
-});
-
-// ruta para guardar
-app.post('/clientes/nuevo', async (req, res) => {
-    try {
-        await fetch('https://mongo-connection-api2.onrender.com/clientes', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(req.body)
-        });
-
-        res.redirect('/clientesvista');
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
 
 
-// buscar cliente por email
-app.get('/clientes/buscar/:email', async (req, res) => {
-    try {
-        const response = await fetch(
-            `https://mongo-connection-api2.onrender.com/clientes/email/${req.params.email}`
-        );
-
-        const data = await response.json();
-
-        res.render('pages/detalleCliente', { cliente: data });
-
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-// Actualizar cliente (PUT)
-// Mostrar formulario con datos
-app.get('/clientes/editar/:email', async (req, res) => {
-    try {
-        const response = await fetch(
-            `https://mongo-connection-api2.onrender.com/clientes/email/${req.params.email}`
-        );
-
-        const data = await response.json();
-
-        res.render('pages/editarCliente', { cliente: data });
-
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-// guardar cambios
-app.post('/clientes/editar/:email', async (req, res) => {
-    try {
-        await fetch(
-            `https://mongo-connection-api2.onrender.com/clientes/email/${req.params.email}`,
-            {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(req.body)
-            }
-        );
-
-        res.redirect('/clientesvista');
-
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-// eliminar cliente
-app.post('/clientes/eliminar/:email', async (req, res) => {
-    try {
-        await fetch(
-            `https://mongo-connection-api2.onrender.com/clientes/email/${req.params.email}`,
-            {
-                method: 'DELETE'
-            }
-        );
-
-        res.redirect('/clientesvista');
-
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-
-app.get('/productosvista', function(req,res){
-   fetch('https://mongo-connection-api2.onrender.com/productos')
-   .then(response => response.json())
-   .then(data => {
-       res.render('pages/productos',
-           {productos:data}
-       )
-   });
-});
-
-
-app.get('/serviciosvista', function(req,res){
-   fetch('https://mongo-connection-api2.onrender.com/servicios')
-   .then(response => response.json())
-   .then(data => {
-       res.render('pages/servicios',
-           {servicios:data}
-       )
-   });
-});
-
-
-
-app.listen(1999);
+app.listen(process.env.PORT || 1999);
